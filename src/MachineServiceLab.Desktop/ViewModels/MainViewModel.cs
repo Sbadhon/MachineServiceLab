013d42a5894e81c29392bcf3616c55a8284dc29c
Sbadhon <sohana89@yahoo.com>
2026-08-27 19:29:32 -0500

    feat: add machine connection workflow

--- src/MachineServiceLab.Desktop/ViewModels/MainViewModel.cs
+++ src/MachineServiceLab.Desktop/ViewModels/MainViewModel.cs
@@ -1,9 +1,25 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
 namespace MachineServiceLab.Desktop.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string ConnectionStatus { get; set; } = "Disconnected";
+
+    public IAsyncRelayCommand ConnectCommand { get; }
+
+    public MainViewModel()
+    {
+        ConnectCommand = new AsyncRelayCommand(async () =>
+        {
+            ConnectionStatus = "Connecting...";
+
+            await Task.Delay(1500);
+
+            ConnectionStatus = "Connected";
+        });
+    }
 }
